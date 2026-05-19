3a92aa344bc5186775a99c880b2816feaaa8119d
faiezv <faiezviljoen@gmail.com>
2026-05-19 16:17:47 +0200

    Initialization of project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="bg-green-800  flex flex-col h-screen justify-center items-center text-5xl">
+      <div className="w-1/2">1</div>
+      <div className="w-1/2">2</div>
+    </div>
+  )
+}
+
+export default App
